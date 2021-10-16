b72fa9535b956c1da57ab8f648b8de35810a214a
June Choe <jchoe001@gmail.com>
2021-10-16 12:20:53 -0400

    test for tracedump funs

--- tests/testthat/test-ggtrace-capture.R
+++ tests/testthat/test-ggtrace-capture.R
@@ -4,6 +4,7 @@ test_that("environment properties are as expected", {
 
   ggtrace:::set_last_ggtrace(NULL)
   expect_null(last_ggtrace())
+  expect_null(clear_global_ggtrace())
 
   boxplot_plot <- ggplot(diamonds[1:500,], aes(cut, depth)) +
     geom_boxplot()
@@ -22,17 +23,13 @@ test_that("environment properties are as expected", {
     trace_exprs = boxplot_trace_exprs,
     print_output = FALSE
   )
-
   invisible(ggplotGrob(boxplot_plot))
   boxplot_tracedump <- last_ggtrace()
 
   # named tracedump
-  expect_equal(
-    names(boxplot_tracedump),
-    names(boxplot_trace_exprs)
-  )
+  expect_equal(names(boxplot_tracedump), names(boxplot_trace_exprs))
 
-    # last 4 are environments
+  # last 4 are environments
   expect_true(
     all(vapply(boxplot_tracedump[-1], rlang::is_environment, logical(1)))
   )
@@ -46,6 +43,7 @@ test_that("environment properties are as expected", {
     rlang::env_label(boxplot_tracedump[["env_start"]]),
     rlang::env_label(boxplot_tracedump[["env_end"]])
   )
+
   # deep copy at end shares same state as the runtime environment at the end
   expect_equal(
     mget(ls(boxplot_tracedump[["env_end"]]), envir = boxplot_tracedump[["env_end"]]),
@@ -80,27 +78,18 @@ test_that("environment properties are as expected", {
   boxplot_tracedump2 <- last_ggtrace()
 
   # new environment created at runtime; not affected by modification to previously retrieved runtime env
-  expect_true(
-    rlang::env_label(boxplot_tracedump2[[1]]) != rlang::env_label(boxplot_tracedump[["env_start"]])
-  )
-  expect_null(
-    boxplot_tracedump2[[1]]$a
-  )
-  expect_true(
-    all(boxplot_tracedump[["env_start"]]$a == 1, boxplot_tracedump[["env_end"]]$a == 1)
-  )
+  expect_false(rlang::env_label(boxplot_tracedump2[[1]]) == rlang::env_label(boxplot_tracedump[["env_start"]]))
+  expect_null(boxplot_tracedump2[[1]]$a)
+  expect_true(all(boxplot_tracedump[["env_start"]]$a == 1, boxplot_tracedump[["env_end"]]$a == 1))
 
   # possible to manually execute steps of the body with the environment
-  expect_true(
-    is.list(boxplot_tracedump2[[2]])
-  )
+  expect_true(is.list(boxplot_tracedump2[[2]]))
   expect_equal(
-    eval(
-      as.list(body(get("compute_panel", Stat)))[[6]],
-      boxplot_tracedump2[[1]]
-    )[,1:12],
+    eval(as.list(body(get("compute_panel", Stat)))[[6]], boxplot_tracedump2[[1]])[,1:12],
     ggplot2:::rbind_dfs(boxplot_tracedump2[[2]])
   )
 
   gguntrace(Stat$compute_panel)
+  expect_null(clear_global_ggtrace())
+
 })
---
+++ tests/testthat/test-last_ggtrace.R
@@ -0,0 +1,80 @@
+library(ggplot2)
+
+violin_plot <- ggplot(iris, aes(Species, Petal.Width)) + geom_violin()
+
+test_that("~step evaluates step", {
+
+  ggtrace:::set_last_ggtrace(NULL)
+  expect_null(last_ggtrace())
+
+  ggtrace(
+    ggplot2:::Layer$compute_statistic,
+    trace_steps = c(4, 4),
+    trace_exprs = list(
+      quote(rlang::env_clone(environment())),
+      quote(~step)
+    ),
+    verbose = FALSE
+  )
+  invisible(ggplotGrob(violin_plot))
+  violin_tracedump <- last_ggtrace()
+
+  violin_eval_manual <- eval(
+    expr = ggbody(ggplot2:::Layer$compute_statistic)[[4]],
+    envir = violin_tracedump[[1]]
+  )
+
+  expect_equal(violin_tracedump[[2]], violin_eval_manual)
+
+})
+
+test_that("exprs evaluated at different steps return different values", {
+
+  ggtrace(
+    StatYdensity$compute_group,
+    trace_steps = c(7, -1),
+    trace_exprs = quote(dens),
+    verbose = FALSE
+  )
+  invisible(ggplotGrob(violin_plot))
+  violin_tracedump <- last_ggtrace()
+
+  expect_false(identical(violin_tracedump[[1]], violin_tracedump[[2]]))
+
+})
+
+test_that("global tracedump can be reset", {
+
+  expect_null(clear_global_ggtrace())
+  expect_null(global_ggtrace())
+
+})
+
+test_that("global tracedump collects until untrace", {
+
+  clear_global_ggtrace()
+  ggtrace(
+    StatYdensity$compute_group,
+    trace_steps = c(7, -1),
+    trace_exprs = quote(dens),
+    once = FALSE,
+    verbose = FALSE
+  )
+  invisible(ggplotGrob(violin_plot))
+  violin_tracedump <- global_ggtrace()
+
+  expect_equal(violin_tracedump[[length(violin_tracedump)]], last_ggtrace())
+  expect_equal(length(violin_tracedump), length(unique(iris$Species)))
+
+  expect_false(any(duplicated(violin_tracedump)))
+  expect_false(any(duplicated(names(violin_tracedump))))
+  expect_false(any(duplicated(unlist(violin_tracedump, recursive = FALSE))))
+
+  gguntrace(StatYdensity$compute_group)
+  invisible(ggplotGrob(violin_plot))
+
+  expect_equal(violin_tracedump, global_ggtrace())
+
+  clear_global_ggtrace()
+
+})
