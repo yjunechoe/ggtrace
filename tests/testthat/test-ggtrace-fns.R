fd8584cf4555816da0d6dd8550481675d2f28394
June Choe <jchoe001@gmail.com>
2021-10-25 23:36:08 -0400

    inspect test

--- tests/testthat/test-ggtrace-fns.R
+++ tests/testthat/test-ggtrace-fns.R
@@ -45,7 +45,7 @@ test_that("basic tracing tests for for custom functions", {
   expect_equal(length(as.list(body(test_fn))), 6)
   expect_equal(test_fn(), 6)
 
-  clear_last_ggtrace()
+  expect_null(clear_last_ggtrace())
   expect_null(last_ggtrace())
   expect_message(ggtrace(test_fn, 2:4, verbose = FALSE), "now being traced")
   invisible(test_fn())
@@ -88,21 +88,40 @@ test_that("basic tracing tests for for custom functions", {
 
 })
 
-# Real world examples
-library(ggplot2)
+test_that("Inspect returns same whether from build or Layer", {
+  # Real world examples
+  library(ggplot2)
 
-# Bar plot using computed/"mapped" aesthetics with `after_stat()` and `after_scale()`
-barplot_plot <- ggplot(data = palmerpenguins::penguins) +
-  geom_bar(
-    mapping = aes(
-      x = species,                           # Discrete x-axis representing species
-      y = after_stat(count / sum(count)),    # Bars represent count of species as proportions
-      color = species,                       # The outline of the bars are colored by species
-      fill = after_scale(alpha(color, 0.5))  # The fill of the bars are lighter than the outline color
-    ),
-    size = 3
-  )
+  # Bar plot using computed/"mapped" aesthetics with `after_stat()` and `after_scale()`
+  barplot_plot <- ggplot(data = palmerpenguins::penguins) +
+    geom_bar(
+      mapping = aes(
+        x = species,                           # Discrete x-axis representing species
+        y = after_stat(count / sum(count)),    # Bars represent count of species as proportions
+        color = species,                       # The outline of the bars are colored by species
+        fill = after_scale(alpha(color, 0.5))  # The fill of the bars are lighter than the outline color
+      ),
+      size = 3
+    )
 
-expect_equal(length(as.list(body(ggplot2:::ggplot_build.ggplot))), 33)
-ggtrace(ggplot2:::ggplot_build.ggplot, 19)
+  clear_global_ggtrace()
+
+  ggtrace(method = ggplot2:::Layer$map_statistic, trace_steps = -1, verbose = FALSE)
+  barplot_plot
+  inside <- last_ggtrace()[[1]]
+
+  expect_equal(length(as.list(body(ggplot2:::ggplot_build.ggplot))), 33)
+  ggtrace(ggplot2:::ggplot_build.ggplot, 19, verbose = FALSE)
+  expect_equal(length(as.list(body(ggplot2:::ggplot_build.ggplot))), 3)
+  barplot_plot
+  outside <- last_ggtrace()[[1]]
+
+  combined <- global_ggtrace()
+
+  expect_equal(inside, combined[[1]][[1]])
+  expect_equal(outside, combined[[2]][[1]])
+  expect_equal(inside, outside[[1]])
+  expect_true(grepl("^ggplot2:::Layer\\$map_statistic", names(combined)[1]))
+  expect_true(grepl("^ggplot2:::ggplot_build\\.ggplot", names(combined)[2]))
+})
 
