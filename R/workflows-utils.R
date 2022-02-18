82f3b9c1b8458eeba779d920000e68fa3ec8a316
June Choe <jchoe001@gmail.com>
2022-02-18 15:59:30 -0500

    export ggdraw_silent

--- R/workflows-utils.R
+++ R/workflows-utils.R
@@ -1,10 +1,8 @@
-#' Simulate printing of a ggplot object without side effects
+#' Simple evaluation and printing of a ggplot object
 #'
-#' Calls `ggplot_gtable(ggplot_build(x))`, which executes all the instructions for plotting `x` without
-#' the side-effects from `ggplot2:::print.ggplot`, namely:
-#'
-#' - `last_plot()` is not overriden
-#' - `grid.draw()` is not called (= nothing gets rendered/drawn)
+#' `ggeval_silent()` evaluates the ggplot object into a `<gtable>` grob and `ggdraw_silent()`
+#' renders the grob with `{grid}`. Combined, these two functions simulate `ggplot2:::print.ggplot`
+#' without overriding `last_plot()`.
 #'
 #' @param x A ggplot
 #'
@@ -14,9 +12,12 @@ ggeval_silent <- function(x) {
   asNamespace("ggplot2")$ggplot_gtable(asNamespace("ggplot2")$ggplot_build(x))
 }
 
+#' @export
+#' @rdname ggeval_silent
 ggdraw_silent <- function(x) {
   asNamespace("grid")$grid.newpage()
   asNamespace("grid")$grid.draw(x)
+  invisible(x)
 }
 
 #' @export
