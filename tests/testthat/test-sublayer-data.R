89825b5f3b172831b8b128675049cd6ac1c75d16
yjunechoe <jchoe001@gmail.com>
2024-10-10 08:17:35 -0400

    more tests for sublayer data

--- tests/testthat/test-sublayer-data.R
+++ tests/testthat/test-sublayer-data.R
@@ -1,10 +1,10 @@
 library(ggplot2)
 
-p <- ggplot(diamonds, aes(cut)) +
-  geom_bar(aes(fill = cut)) +
-  stat_count(geom = "label", aes(label = after_stat(count)))
-
 test_that("sublayer data targeting works", {
+
+  p <- ggplot(diamonds, aes(cut)) +
+    geom_bar(aes(fill = cut)) +
+    stat_count(geom = "label", aes(label = after_stat(count)))
 
   expect_in(
     c("x", "PANEL", "group"),
@@ -25,5 +25,37 @@ test_that("sublayer data targeting works", {
     c("alpha", "PANEL", "group"),
     colnames(layer_after_scale(p))
   )
+
+})
+
+test_that("sublayer data can target layer by index", {
+
+  p <- ggplot(diamonds) +
+    geom_bar(aes(cut, fill = cut)) +
+    geom_blank() +
+    stat_count(geom = "label", aes(cut, label = after_stat(count)))
+
+  all_stages <- list(
+    layer_before_stat(p, 2),
+    layer_after_stat(p, 2),
+    layer_before_geom(p, 2),
+    layer_after_scale(p, 2)
+  )
+
+  expect_identical(
+    Reduce(function(x, y) if (identical(x, y)) x else FALSE, all_stages),
+    all_stages[[1]]
+  )
+
+  expect_no_error({
+    layer_before_stat(p, 1)
+    layer_before_stat(p, 2)
+    layer_before_stat(p, 3)
+  })
+
+  expect_error({
+    layer_before_stat(p, 0)
+    layer_before_stat(p, 4)
+  })
 
 })
